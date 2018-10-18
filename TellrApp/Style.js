36b82a2e10a8d4cba097efdc2c65b91b5ab910a8
guohanting <guo.hanting@gmail.com>
2018-10-18 06:35:38 -0400

    updated styling

---
+++ TellrApp/Style.js
@@ -0,0 +1,29 @@
+
+import { StyleSheet } from 'react-native';
+
+var Style = StyleSheet.create({
+    rootContainer: {
+        flex: 1
+    },
+
+    displayContainer: {
+        flex: 2,
+        backgroundColor: '#411151',
+        justifyContent: 'center'
+    },
+
+    inputContainer: {
+        flex: 8,
+        backgroundColor: '#BB205A'
+    },
+
+    displayText: {
+        color: 'red',
+        fontSize: 38,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 20
+    },
+});
+
+export default Style;
