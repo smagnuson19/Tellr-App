0296e0ad7fee2267d2c08c5b48f1cf89b1c853b4
scott <scott.a.magnuson.19@dartmouth.edu>
2018-10-21 22:00:33 -0400

    Frontend hitting an API

--- TellrApp/App.js
+++ TellrApp/App.js
@@ -1,46 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { NativeRouter as Router, Route, Switch } from "react-router-native";
 import Style from './Style';
+import BottomTabBar from './navigation/bottom_tab_bar';
+import Home from './components/home';
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={Style.rootContainer}>
-          <View style={Style.displayContainer}>
-            <Text style={Style.displayText}>Hello World! </Text>
-          </View>
-          <View style={Style.inputContainer}>
-            <Text style={styles.welcome}>This will be the beginning of the Tellr scaffold</Text>
-          </View>
-      </View>
+// type Props = {};
+
+
+const LandingPage = (props) => {
+  return (
+        <Home />
     );
-  }
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  //  <BottomTabBar /> - put this in below, was buggy error
+const App = (props) => {
+  return (
+    <Router>
+      <View style={Style.rootContainer}>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </View>
+    </Router>
+  );
+};
+
+export default App;
