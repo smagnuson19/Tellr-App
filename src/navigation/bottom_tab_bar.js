3523d86c9c492ebb541fb51df2d28a816d8c851c
scott <scott.a.magnuson.19@dartmouth.edu>
2018-10-28 19:09:03 -0400

    tab bar working

--- src/navigation/bottom_tab_bar.js
+++ src/navigation/bottom_tab_bar.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation';
 import { View, Text } from 'react-native';
+// import Ionicons from 'react-native-vector-icons/FontAwesome';
+import Home from '../components/home';
 
-const HomeTab = (props) => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Home</Text></View>;
-};
 
 const PaymentsTab = (props) => {
   return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Payments</Text></View>;
 };
-
-const AddTaskTab = (props) => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Add Task</Text></View>;
-};
-
-const ManageTab = (props) => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Manage</Text></View>;
-};
-
-const ProfileTab = (props) => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Profile</Text></View>;
-};
+//
+// const AddTaskTab = (props) => {
+//   return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Add Task</Text></View>;
+// };
+//
+// const ManageTab = (props) => {
+//   return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Manage</Text></View>;
+// };
+//
+// const ProfileTab = (props) => {
+//   return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Profile</Text></View>;
+// };
 
 
 const MainTabBar = createBottomTabNavigator({
-  HomeTab,
+  Home,
   PaymentsTab,
-  AddTaskTab,
-  ManageTab,
-  ProfileTab,
-}, {
-  initialRouteName: 'HomeTab',
+},
+{
+  initialRouteName: 'Home',
 });
 
 
