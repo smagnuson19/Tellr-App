025328ea199d086d46528d74f23ee1a5a6a448d1
Emily K Pitts <emilykpitts@Emilys-MacBook-Air-5.local>
2018-11-12 16:39:10 -0500

    error message for addTask

--- src/components/tabs/chores.js
+++ src/components/tabs/chores.js
@@ -12,17 +12,18 @@ import { colors, fonts } from '../../styling/base';
 
 const ROOT_URL = 'http://localhost:5000/api';
 const API_KEY_TASKS = 'childtasks';
+const API_KEY_COMPLETED = 'tasks/completed';
 
 class Chores extends Component {
   constructor(props) {
     super(props);
     this.state = {
       taskList: [],
-      taskName: '',
-      taskDeadline: '',
-      taskDescription: '',
-      reward: '',
-      senderEmail: '',
+      // taskName: '',
+      // taskDeadline: '',
+      // taskDescription: '',
+      // reward: '',
+      // senderEmail: '',
       childEmail: '',
     };
   }
@@ -34,21 +35,42 @@ class Chores extends Component {
   fetchNames() {
     AsyncStorage.getItem('emailID', (err, result) => {
       const API_KEY_USERS = result;
-      console.log(API_KEY_USERS, 'result here');
-      // this.setState({ childEmail: API_KEY_USERS });
+      this.setState({ childEmail: API_KEY_USERS });
       return axios.get(`${ROOT_URL}/${API_KEY_TASKS}/${API_KEY_USERS}`).then((response) => {
         const taskInfo = response.data;
-        console.log(taskInfo, 'task info here');
         // loop through each task and set the states
         const taskListTemp = [];
         Object.keys(taskInfo).forEach((key) => {
           taskListTemp.push({ label: taskInfo[key].taskName, value: taskInfo[key] });
         });
         this.setState({ taskList: taskListTemp });
+        console.log(this.state.taskList, 'taskList');
       }).catch((error) => {
         console.log('ERROR in Chores');
       });
     });
+  }
+
+  submitChore(taskNameSubmitted) {
+    // So that you are unable to navigate back to login page once logged in.
+    // const resetAction = StackActions.reset({
+    //   index: 0, // <-- currect active route from actions array
+    //   key: null,
+    //   actions: [
+    //     NavigationActions.navigate({ routeName: 'ParentTabBar' }),
+    //   ],
+    // });
+
+    const payLoad = {
+      taskName: taskNameSubmitted,
+      childEmail: this.state.childEmail,
+    };
+
+    axios.post(`${ROOT_URL}/${API_KEY_COMPLETED}`, { payLoad })
+      .then((response) => {
+        console.log(response.data);
+        // this.props.navigation.dispatch(resetAction);
+      });
   }
 
   render() {
@@ -60,18 +82,30 @@ class Chores extends Component {
               <Text style={Style.headerText}>Chores </Text>
             </View>
             <View style={Style.listContainer}>
-              <Text style={choreStyle.taskNameStyle}> Take out Trash </Text>
-              <Text style={choreStyle.deadlineStyle}>
+              <Text style={choreStyle.taskNameStyle}>
                 {' '}
-                {'Due 11/26 at 4pm'}
+                {'this.state.taskList[0].value.taskName'}
                 {' '}
               </Text>
-              <Text style={Style.displayText}> Please take out the trash from the Kitchen </Text>
-              <Text style={choreStyle.rewardStyle}> Reward money </Text>
+              <Text style={choreStyle.deadlineStyle}>
+                {'Due: '}
+                {'this.state.taskList[0].value.taskDeadline'}
+                {' '}
+              </Text>
+              <Text style={Style.displayText}>
+                {' '}
+                {'this.state.taskList[0].value.taskDescription'}
+                {' '}
+              </Text>
+              <Text style={choreStyle.rewardStyle}>
+                {'Reward: '}
+                {'this.state.taskList[0].value.reward'}
+                {' '}
+              </Text>
               <Button
                 title="Done!"
                 rounded
-                // onPress={() => this.submitChore()}
+                // onPress={() => this.submitChore(this.state.taskList[0].value.taskName)}
                 style={choreStyle.doneButtonStyle}
               />
             </View>
@@ -86,25 +120,22 @@ const choreStyle = StyleSheet.create({
   taskNameStyle: {
     fontSize: fonts.md,
     color: 'white',
-    marginLeft: -20,
+    marginLeft: -22,
     fontFamily: fonts.secondary,
-    // fontWeight: 'bold',
     textAlign: 'left',
   },
   deadlineStyle: {
     fontSize: fonts.smmd,
     color: 'white',
-    marginRight: -20,
+    marginLeft: -20,
     fontFamily: fonts.secondary,
-    // fontWeight: 'bold',
-    textAlign: 'right',
+    textAlign: 'left',
   },
   rewardStyle: {
     fontSize: fonts.smmd,
     color: 'white',
     marginRight: -20,
     fontFamily: fonts.secondary,
-    // fontWeight: 'bold',
     textAlign: 'right',
   },
   doneButtonStyle: {
