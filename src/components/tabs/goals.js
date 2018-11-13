f8f0cbe625741574cad051e14516f1a4a4d17fa4
jedrosen <samuel.j.rosen.19@dartmouth.edu>
2018-11-13 03:36:05 -0500

    Goals not hardcoded

--- src/components/tabs/goals.js
+++ src/components/tabs/goals.js
@@ -23,34 +23,13 @@ class Goals extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
+      // loading: true,
       Goals: '',
       Balance: '',
       senderEmail: '',
     };
     this.redeemAction = this.redeemAction.bind(this);
   }
-
-  // fetchGoals() {
-  //   AsyncStorage.getItem('emailID', (err, result) => {
-  //     const API_KEY_USERS = result;
-  //     return axios.get(`${ROOT_URL}/goals/${API_KEY_USERS}`).then((response) => {
-  //       // make a list of the childs goals
-  //       const gList = response.data;
-  //       const goalList = [];
-  //       // loop through each kid and make an object for them with FirstName, Email
-  //       Object.keys(gList).forEach((key) => {
-  //         if (gList[key].approved === true && gList[key].redeemed !== false) {
-  //           goalList.push({
-  //             goalName: gList[key].goalName, goalDescription: gList[key].goalDescription, goalValue: gList[key].value, goalImage: gList[key].image,
-  //           });
-  //         }
-  //       });
-  //     }).catch((error) => {
-  //       console.log('ERROR in AddTask');
-  //     });
-  //   });
-
 
   componentWillMount() {
     console.log('Fetching info');
@@ -62,16 +41,18 @@ class Goals extends Component {
     }).catch((error) => {
       console.log('ERROR in NewGoal');
     });
-    function sleep(time) {
-      return new Promise(resolve => setTimeout(resolve, time));
-    }
-    sleep(100).then(() => {
-    // Do something after the sleep!
-      console.log('Getting Goals after sleep');
-    });
+    // function sleep(time) {
+    //   return new Promise(resolve => setTimeout(resolve, time));
+    // }
+    // sleep(800).then(() => {
+    // // Do something after the sleep!
+    //   console.log('Getting Goals after sleep');
+    // });
+  }
 
+  fetchGoals() {
     console.log(this.state.senderEmail);
-    return axios.get(`${ROOT_URL}/goals/jed@jed.com`).then((response) => {
+    return axios.get(`${ROOT_URL}/goals/${this.state.senderEmail}`).then((response) => {
       // make a list of the parent's children
       console.log('Dealing With Response');
       const gList = response.data;
@@ -93,7 +74,6 @@ class Goals extends Component {
       });// end for each
       this.setState({ Goals: goalList });
       console.log('Got Goals');
-      this.setState({ loading: goalList });
     }).catch((error) => {
       console.log('ERROR in getGoals');
     });
@@ -102,75 +82,24 @@ class Goals extends Component {
   redeemAction(action) {
     console.log('Button Pressed to redeem');
   }
-  // getGoals() {
-  //   console.log(this.state.senderEmail);
-  //   return axios.get(`${ROOT_URL}/goals/jed@jed.com`).then((response) => {
-  //     // make a list of the parent's children
-  //     console.log('Dealing With Response');
-  //     const gList = response.data;
-  //     const goalList = [];
-  //     // loop through each kid and make an object for them with FirstName, Email
-  //     Object.keys(gList).forEach((key) => {
-  //       if (gList[key].approved !== 30 && gList[key].redeemed === false) {
-  //         goalList.push({
-  //           key,
-  //           goalName: gList[key].name,
-  //           goalValue: gList[key].value,
-  //           goalDescription: gList[key].description,
-  //           goalImage: gList[key].image,
-  //         });
-  //       } else {
-  //         console.log('Not Approved Goal');
-  //       }// end if
-  //     });// end for each
-  //     this.setState({ Goals: goalList });
-  //     console.log('Got Goals');
-  //   }).catch((error) => {
-  //     console.log('ERROR in getGoals');
-  //   });
-  // }
-
-
-  // submitgoals() {
-  //   const payLoad = {
-  //     goalsName: this.state.goalsName,
-  //     goalsDeadline: this.state.goalsDeadline,
-  //     child: this.state.child,
-  //     goalsDescription: this.state.goalsDescription,
-  //     reward: this.state.reward,
-  //   };
-  //
-  //   axios.post(`${ROOT_URL}`, { payLoad })
-  //     .then((response) => {
-  //       console.log(response.data);
-  //     });
-  //
-  //   // So that you are unable to navigate back to login page once logged in.
-  //   const resetAction = StackActions.reset({
-  //     index: 0, // <-- currect active route from actions array
-  //     key: null,
-  //     actions: [
-  //       NavigationActions.navigate({ routeName: 'MainTabBar' }),
-  //     ],
-  //   });
-  //
-  //   this.props.navigation.dispatch(resetAction);
-  // }
 
   render() {
     const balanceString0 = 'Your Balance: $';
     const balanceString = `${balanceString0} ${this.state.Balance}`;
-    if (this.state.loading === true) {
+    if (typeof this.state.Goals === 'undefined' || this.state.Goals.length === 0) {
+      console.log('No');
+      this.fetchGoals();
       return (
         <View style={Style.rootContainer}>
           <LinearGradient colors={['rgba(4, 27, 37, 0.9615)', 'rgba(1, 6, 3, 0.76)']} style={Style.gradient}>
             <View style={Style.displayContainer}>
-              <Text style={Style.headerText}>Loading</Text>
+              <Text style={Style.headerText}>Loading, Please wait for goals</Text>
             </View>
           </LinearGradient>
         </View>
       );
     } else {
+      console.log('Yes');
       return (
         <View style={Style.rootContainer}>
           <LinearGradient colors={['rgba(4, 27, 37, 0.9615)', 'rgba(1, 6, 3, 0.76)']} style={Style.gradient}>
