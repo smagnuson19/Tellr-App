39d684177928d0cdadd5e784022df160f3996957
scott <scott.a.magnuson.19@dartmouth.edu>
2019-02-03 16:34:09 -0500

    added support for pushNotifications

--- android/app/src/main/java/com/tellrapp/MainApplication.java
+++ android/app/src/main/java/com/tellrapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.tellrapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new RNSoundPackage(),
--- src/pushNotifications/index.js
+++ src/pushNotifications/index.js
@@ -1,23 +1,7 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
 
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
-import OneSignal from 'react-native-onesignal';
-import RootStack from './navigation/navigation';
-import reducers from './reducers';
-
-
-const store = createStore(reducers, {}, compose(
-  applyMiddleware(thunk),
-));
-
-
-// disable really annoying in app warnings
-console.disableYellowBox = true;
-//  <BottomTabBar /> - put this in below, was buggy error
-
-export default class App extends Component {
+class PushNotifications extends Component {
   constructor(properties) {
     super(properties);
     // need to setup app first
@@ -48,12 +32,5 @@ export default class App extends Component {
   onIds(device) {
     console.log('Device info: ', device);
   }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <RootStack />
-      </Provider>
-    );
-  }
 }
+export default PushNotifications;
