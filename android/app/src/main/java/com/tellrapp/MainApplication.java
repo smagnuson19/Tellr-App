150ff8cf8391df17c773457b4e2091307d2e2816
scott <scott.a.magnuson.19@dartmouth.edu>
2019-02-13 21:27:13 -0500

    updates

--- android/app/src/main/java/com/tellrapp/MainApplication.java
+++ android/app/src/main/java/com/tellrapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.tellrapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bugsnag.BugsnagReactNative;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            BugsnagReactNative.getPackage(),
             new ReactNativeOneSignalPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /** @format */
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+
+const bugsnag = new Client('93454d19034bb201dac58d2174490e78');
+bugsnag.notify(new Error('Test error'));
 
 AppRegistry.registerComponent(appName, () => App);
